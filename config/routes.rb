c451cbcc8e4f825e3c0c98f634e4aff2ea74d11d
amandaseasholtz <alseasholtz@valdosta.edu>
2019-03-18 21:54:25 -0400

    Commiting routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+    devise_for :accounts
+    root 'homepage#index', as: 'homepage_index'
+    get 'homepage/index'
+    get 'say/request'
+    get 'say/history'
+    get 'say/info'
+    get 'say/post'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
+  
