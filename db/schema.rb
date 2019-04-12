0666d381b6ce87e57279127053b8c3b03cf628ef
jose <delgadoj23418@gmail.com>
2019-04-12 02:27:14 -0400

    Generated scaffold for departments and added to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_160240) do
+ActiveRecord::Schema.define(version: 2019_04_12_062349) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 2019_04_03_160240) do
     t.string "bid"
     t.string "department"
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.string "did"
+    t.string "dname"
+    t.decimal "dbudget"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -86,6 +94,7 @@ ActiveRecord::Schema.define(version: 2019_04_03_160240) do
     t.string "payment_information"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "super_accounts", force: :cascade do |t|
