9620755be5542bc90670fe70d136b684d9b123b1
Savon Jackson <savon9ijackzon@gmail.com>
2019-04-14 13:54:48 -0400

    change request

--- app/models/post_travel.rb
+++ app/models/post_travel.rb
@@ -1,2 +1,4 @@
 class PostTravel < ApplicationRecord
+  belongs_to :employee, optional: true
+  belongs_to :budget, optional: true
 end
--- db/migrate/20190402023014_create_requests.rb
+++ db/migrate/20190402023014_create_requests.rb
@@ -3,12 +3,13 @@ class CreateRequests < ActiveRecord::Migration[5.2]
     create_table :requests do |t|
       t.string :name
       t.string :destination
-      t.string :requested_travel_days
+      t.date :start_date
+      t.date :end_date
 
 
       t.string :employ_department
 
-      
+
       t.string :budget_department
       t.string :purpose
       t.string :expected_expenses
@@ -18,7 +19,7 @@ class CreateRequests < ActiveRecord::Migration[5.2]
 
       t.references :employee, foreign_key: true
       t.references :budget, foreign_key: true
-      
+
 
       t.timestamps
     end
