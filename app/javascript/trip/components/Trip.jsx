e003c773a4603eeda8b78e4b557de9013741c109
amandaseasholtz <alseasholtz@valdosta.edu>
2019-04-20 09:41:29 -0400

    Fixed Trip.jsx

--- app/javascript/trip/components/Trip.jsx
+++ app/javascript/trip/components/Trip.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types'
 
-export default class Trip extends React.Component {
+export default class Catalog extends React.Component {
 
+    state = { trips: [] };
+
+    componentDidMount = () => {
+        var self = this;
+
+        axios.defaults.headers.common['X-Requested-With'] = "XMLHttpRequest";
+        axios.get('/')
+            .then(function (response) {
+                console.log(response.data);
+                self.setState({ books: response.data })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    };  
 
     render = () => {
         return(
             <div>
                 <h3>
-                    There are {this.state.trips} books in the catalog.
+                    There are {this.state.trips.length} books in the catalog.
                 </h3>
             </div>
         );
     };
+   
 }   
